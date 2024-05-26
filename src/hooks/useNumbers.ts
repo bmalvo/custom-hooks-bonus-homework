8adac625b579bdfb93b6785da87d8920f8eff52f
bmalvo <boydmalvo@gmail.com>
2024-05-26 12:58:33 +0200

    first commit

---
+++ src/hooks/useNumbers.ts
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+export const useNumbers = () => {
+  const [numbers, setNumbers] = useState<number[]>([])
+
+  const generateNumber = () => {
+    const newNumber = Math.round(Math.random() * 100)
+    setNumbers((prevNumbers) => [...prevNumbers, newNumber])
+  }
+
+  const deleteNumber = (indexToDelete: number) => {
+    setNumbers((prevNumbers) =>
+      prevNumbers.filter((_el, index) => index !== indexToDelete),
+    )
+  }
+
+  return {
+    numbers,
+    generateNumber,
+    deleteNumber,
+  }
+}
